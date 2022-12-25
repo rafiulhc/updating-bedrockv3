f0414540ae6b50574a715c76edbb465440461cdc
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-12-25 11:35:29 +0600

    balance token update

--- src/views/Swap/view/_BL.tsx
+++ src/views/Swap/view/_BL.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { useIsMobile } from "../../../hooks/useIsMobile";
 import useWindowSize from "../../../hooks/useWindowSize";
-import {
-  CoinSelection,
-  FormContext,
-  IFormContextValue,
-} from "../contexts/FormContext";
+import { CoinSelection, FormContext } from "../contexts/FormContext";
 
 export interface Coin {
   symbol: string;
@@ -16,13 +12,13 @@ export interface Coin {
 }
 
 export type Field = "1" | "2";
-//For Mainnet
+
 export const coins: Coin[] = [
-  {
+    {
     symbol: "BNB",
     name: "Binance",
     img: "https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png?v=637697418070000000",
-    address: "0x51a4c0010B25d8bDB6e3B3F73FEfF3774c43F0F2",
+    address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     decimals: 18,
   },
   {
@@ -33,17 +29,17 @@ export const coins: Coin[] = [
     decimals: 18,
   },
   {
-    symbol: "WBNB",
-    name: "Binance",
-    img: "https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png?v=637697418070000000",
-    address: "0x51a4c0010B25d8bDB6e3B3F73FEfF3774c43F0F2",
-    decimals: 18,
-  },
-  {
     symbol: "BUSD",
     name: "BUSD Token",
     img: "https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png",
     address: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+    decimals: 18,
+  },
+  {
+    symbol: "WBNB",
+    name: "Binance",
+    img: "https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png?v=637697418070000000",
+    address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     decimals: 18,
   },
   {
@@ -53,51 +49,19 @@ export const coins: Coin[] = [
     address: "0x55d398326f99059fF775485246999027B3197955",
     decimals: 18,
   },
+  {
+    symbol: "USDC",
+    name: "USD Coin",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+    address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+    decimals: 18,
+  }
 ];
 
-//For Testnet(Rinkeby)
-// export const coins: Coin[] = [
-//   {
-//     symbol: "BNB",
-//     name: "Binance",
-//     img: "https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png?v=637697418070000000",
-//     address: "0x51a4c0010B25d8bDB6e3B3F73FEfF3774c43F0F2",
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "BN",
-//     name: "BN Token",
-//     img: "https://s2.coinmarketcap.com/static/img/coins/200x200/15842.png",
-//     address: "0x8fE08840A02556081EDd8b591E3323f01D288348",
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "WBNB",
-//     name: "Binance",
-//     img: "https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png?v=637697418070000000",
-//     address: "0x51a4c0010B25d8bDB6e3B3F73FEfF3774c43F0F2",
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "EPT",
-//     name: "Bitcoin",
-//     img: "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png",
-//     address: "0xDACfD846191a4557f7C848eb8bE2C7469579D0CF",
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "LPT",
-//     name: "Etherium",
-//     img: "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png",
-//     address: "0xC9c6afaef0b8bC9384841fF9c43937060B3f5227",
-//     decimals: 6,
-//   },
-// ];
 export const useBL = () => {
   const { width } = useWindowSize();
   const formContext = React.useContext(FormContext);
-  const {isMobile} = useIsMobile();
-
+  const { isMobile } = useIsMobile();
 
   const setVal = (val: string, field: Field, val1: string) => {
     if (field === "1") {
@@ -158,3 +122,4 @@ export const useBL = () => {
 
   return { coins, formContext, setVal, setCoin, swap, isMobile };
 };
+
