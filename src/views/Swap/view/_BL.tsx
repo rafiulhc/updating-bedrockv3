ad1c82f511eeea8d15912b3246a952f78f9e1f2e
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-12-29 10:03:29 +0600

    updated number of token

--- src/views/Swap/view/_BL.tsx
+++ src/views/Swap/view/_BL.tsx
@@ -55,7 +55,57 @@ export const coins: Coin[] = [
     img: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
     address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
     decimals: 18,
-  }
+  },
+  {
+    symbol: "BTCB",
+    name: "Binance BTC",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
+    address: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+    decimals: 18,
+  },
+  {
+    symbol: "ETH",
+    name: "Ethereum",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+    address: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    decimals: 18,
+  },
+  {
+    symbol: "DOGE",
+    name: "Dogecoin",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/74.png",
+    address: "0xbA2aE424d960c26247Dd6c32edC70B295c744C43",
+    decimals: 8,
+  },
+  {
+    symbol: "ADA",
+    name: "Cardano",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png",
+    address: "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47",
+    decimals: 18,
+  },
+  {
+    symbol: "MATIC",
+    name: "Matic Token",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png",
+    address: "0xCC42724C6683B7E57334c4E856f4c9965ED682bD",
+    decimals: 18,
+  },
+  {
+    symbol: "DAI",
+    name: "Dai Token",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png",
+    address: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
+    decimals: 18,
+  },
+  {
+    symbol: "Dot",
+    name: "Polkadot",
+    img: "https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png",
+    address: "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402",
+    decimals: 18,
+  },
+
 ];
 
 export const useBL = () => {
@@ -91,18 +141,18 @@ export const useBL = () => {
     if (field === "1") {
       formContext.setForm({
         form: {
-          field1: {
+          field2: {
             ...formContext.form.field1,
             coin: { ...coin },
           },
-          field2: { ...formContext.form.field2 },
+          field1: { ...formContext.form.field2 },
         },
       });
     } else if (field === "2") {
       formContext.setForm({
         form: {
-          field1: { ...formContext.form.field1 },
-          field2: {
+          field2: { ...formContext.form.field1 },
+          field1: {
             ...formContext.form.field2,
             coin: { ...coin },
           },
@@ -122,4 +172,3 @@ export const useBL = () => {
 
   return { coins, formContext, setVal, setCoin, swap, isMobile };
 };
-
